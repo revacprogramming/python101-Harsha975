5a94f9d09c24e886d0961aee6ce4f251ed2df671
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-05-21 15:52:35 +0000

    harsha

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,24 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+# filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+names={}
+name=[]
+for line in handle:
+    if line.startswith('From '):
+        line=line.split()
+        name=line[1]
+    	if name not in names :
+        	names[name]=1
+    	else:
+        	names[name]=names[name]+1
+            
+values=names.values()
+max_value=max(values)
+for key,value in names.items():
+    if max_value==value:
+       nm=key
+print(nm,max_value)
