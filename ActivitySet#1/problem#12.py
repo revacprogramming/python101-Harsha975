c4240c24f43fc76386e6f8a61ba7a2d56e599034
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-05-29 11:06:48 +0000

    harsha

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,11 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+d=0
+f=open('mydat','r')
+for line in f:
+    g=re.findall('\d+',line)
+    if g!=None:
+        for i in g:
+            d+=int(i)
+print(d)
