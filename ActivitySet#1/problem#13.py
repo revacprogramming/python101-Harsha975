3b46b98d9867870af1681f30c1eea6bf9e9e7675
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-05-31 16:51:16 +0000

    harsha

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,13 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+import socket
+# socket=socket.socket()
+socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect(('data.pr4e.org', 80))
+call='GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+socket.send(call)
+while True:
+    data=socket.recv(1000)
+    if len(data)<1:
+        break
+    print(data.decode())
