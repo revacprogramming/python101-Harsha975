2315c316bfc9128faa312ee507c7c240bbf329b1
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-06-05 04:08:54 +0000

    harsha

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,12 +1,18 @@
 
-
 class Menu:
-  def add(self,price)
-     
+    # def __init__(self,food,price):
+    #     self.food=food
+    #     self.price=price
+    def add(self,food,price):
+        self.food=food
+        self.price=price
+    def show(self):
+        print(self.food, self.price)
 
 
-m = Menu()  # Menu is a class
+
+m = Menu()
 m.add("idly", 10)
+m.show()
 m.add("vada", 20)
-
 m.show()
