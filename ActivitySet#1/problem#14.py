ead8df838e82c5a38115ca7c33449c4e971af731
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-06-11 12:22:40 +0000

    harsha

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,8 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import urllib.request
+import xml.etree.ElementTree as et
+from urllib.request import*
+url=input("enter the url :")
+# data=urllib.request.urlopen(url)
+tree=et.fromstring(data)
