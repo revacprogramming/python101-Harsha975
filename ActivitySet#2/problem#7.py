8d8b2f47704b641a4e88446c6e6aa9b0798fdbcb
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-06-26 13:53:31 +0000

    harsha

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,14 +1,13 @@
-
-
 class Menu:
-  def __init__(self):
-    self.list=[]
-  def __add__(self,a):
-    self.list.append((a[0],a[1]))
-    return self
-  def __str__(self)
-    
-  
+    def __init__(self):
+        self.list = []
+
+    def __add__(self, a):
+        self.list.append((a[0], a[1]))
+        return self
+
+    def __str__(self):
+        return '{self.list[0][0]} {self.list[0][1]} \n{self.list[1][0]} {self.list[1][1]} '.format(self=self)
 
 
 m = Menu()
