dd0ac9b3ad350e8ff18ebb87a61d4b12415db63f
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-02 14:55:02 +0000

    harsha

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -2,7 +2,7 @@
 
 class Menu:
   def __setitem__(self,key,value):
-    self.__dict__[keys]=value
+    self.__dict__[key]=value
   def __str__(self):
     return  ('\n'.join([str(a+' '+str(b)) for a,b in self.__dict__.items()]))
 
