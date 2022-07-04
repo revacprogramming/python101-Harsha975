b068c5f006e4acb3aec2836edc139464b6df7cce
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-04 04:57:20 +0000

    web_01_..completed

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -2,7 +2,13 @@
 # https://www.py4e.com/lessons/servces
 import urllib.request
 import xml.etree.ElementTree as et
-from urllib.request import*
-url=input("enter the url :")
-# data=urllib.request.urlopen(url)
-tree=et.fromstring(data).
+fh=urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1550210.xml')
+data=fh.read()
+tree=et.fromstring(data)
+lst=tree.findall('comments/comment') 
+sum=0
+for items in lst:
+    num=int(items.find('.//count').text)
+    sum+=num
+
+print(sum)
