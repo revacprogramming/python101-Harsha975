31413fbe4a3d29f89a58e9cc8c26607fd4357b03
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-07 14:04:04 +0000

    harsha

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,5 +1,6 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+'''01'''
 import urllib.request
 import xml.etree.ElementTree as et
 fh=urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1550210.xml')
@@ -11,4 +12,14 @@ for items in lst:
     num=int(items.find('.//count').text)
     sum+=num
 
+print(sum)
+'''02'''
+from urllib.request import urlopen
+import json
+url='http://py4e-data.dr-chuck.net/comments_1550211.json'
+jsFile = urlopen(url)
+data =json.loads(jsFile.read())
+sum=0
+for i in data['comments']:
+    sum+=i['count']
 print(sum)
