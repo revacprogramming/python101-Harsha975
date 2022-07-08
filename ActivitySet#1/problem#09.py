f6ad48c1a595a2bec46f168a7ae97ba8ca555932
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-08 16:16:57 +0000

    ..

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -8,18 +8,3 @@ for line in fh:
     for word in line.split():
         if word not in lst :
         	lst.append(word)
-'''02'''
-# fname = input("Enter file name: ")
-# if len(fname) < 1:
-#     fname = "mbox-short.txt"
-
-# fh = open(fname)
-# count = 0
-
-# for line in fh :
-#     if line.startswith("From "):
-#         #if line[4]==' ':
-#          #   line=line.split()
-#             print(line.split()[1])
-#             count+=1
-# print("There were", count, "lines in the file with From as the first word")
