b80d65da90c7f7b3789169880d4f1d271b2e1636
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-16 14:26:35 +0000

    .

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -4,13 +4,9 @@
 
 count=0
 total=0
-fname = input("Enter file name: ")
-fh = open(fname,'r')
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:"): 
-        continue
-    count=count+1
-    index=line.find("0.")
-    float_no=float(line[index:])
-    total+=float_no
-print("Average spam confidence:",total/count)
+with open(input('enter the file '), 'r') as f:
+    for line in f:
+        if line.startswith('X-DSPAM-Confidence:'):
+            total+=float(line[line.find('0.'):])
+            count+=1           
+print('Average spam confidence:',total/count)
