a9612c6c2357d9069697820ec2e71158ae8ac0ec
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-17 17:24:37 +0000

    .

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,24 +1,32 @@
 # Dictionaries
 
 # filename = "dataset/mbox-short.txt"
-name = input("Enter file:")
-if len(name) < 1:
-    name = "mbox-short.txt"
-handle = open(name)
-names={}
-name=[]
-for line in handle:
-    if line.startswith('From '):
-        line=line.split()
-        name=line[1]
-    	if name not in names :
-        	names[name]=1
-    	else:
-        	names[name]=names[name]+1
+# name = input("Enter file:")
+# if len(name) < 1:
+#     name = "mbox-short.txt"
+# handle = open(name)
+# names={}
+# name=[]
+# for line in handle:
+#     if line.startswith('From '):
+#         line=line.split()
+#         name=line[1]
+#     	if name not in names :
+#         	names[name]=1
+#     	else:
+#         	names[name]=names[name]+1
             
-values=names.values()
-max_value=max(values)
-for key,value in names.items():
-    if max_value==value:
-       nm=key
-print(nm,max_value)
+# values=names.values()
+# max_value=max(values)
+# for key,value in names.items():
+#     if max_value==value:
+#        nm=key
+# print(nm,max_value
+print('=====================================')
+dic_t={}
+with open(input('enter the file :'),'r') as f:
+    for line in f:
+        if line.startswith('From '):
+            email=line.split()
+            dic_t[email[1]]=dic_t.get(email[1],0)+1
+print(dict((b,a) for a,b in dic_t.items()).get(max(dic_t.values())),max(dic_t.values()))
