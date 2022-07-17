c028ab6d7b4b001e10207d5d5802bbbba491f980
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-17 17:07:15 +0000

    .

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -2,6 +2,18 @@
 
 #filename = "dataset/mbox-short.txt"
 
+# count=0
+# total=0
+# fname = input("Enter file name: ")
+# fh = open(fname,'r')
+# for line in fh:
+#     if not line.startswith("X-DSPAM-Confidence:"): 
+#         continue
+#     count=count+1
+#     index=line.find("0.")
+#     float_no=float(line[index:])
+#     total+=float_no
+# print("Average spam confidence:",total/count)
 count=0
 total=0
 with open(input('enter the file '), 'r') as f:
--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -8,3 +8,17 @@ for line in fh:
     for word in line.split():
         if word not in lst :
         	lst.append(word)
+fh.close()
+'''02'''
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for line in fh :
+    if line.startswith("From "):
+            print(line.split()[1])
+            count+=1
+print("There were", count, "lines in the file with From as the first word")
+fh.close()
