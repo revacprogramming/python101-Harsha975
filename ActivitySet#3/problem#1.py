93aa151f9a33337a95d2e8306765bfa93f24626d
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2022-07-19 14:28:38 +0000

    .

--- ActivitySet#3/problem#1.py
+++ ActivitySet#3/problem#1.py
@@ -24,13 +24,15 @@ def area(x1,x2,x3,y1,y2,y3):
     
 def main():
     areas=[]
-
+    sides=[]
     n=int(input('enter the number of trinagle u want:'))
     for i in range(0,n):
         x1,y1,x2,y2,x3,y3=map(float,input('enter the coordinate(x1,y1) ,(x2,y2) ,(x3,y3) of the rectangle:').split())
         area_=area(x1,x2,x3,y1,y2,y3)
+        sides.append((x1,y1,x2,y2,x3,y3))
         areas.append(area_)
-    for i in range(n):
-        print('Area of rectangle with vertices ({},{}) ({},{}) ({},{}) is {}'.format(x1,y1,x2,y2,x3,y3,areas[i]))  
-    print('TATA TATA Bye bye!===========')
+    print(sides)
+    j=0  
+    for j in range(0,n):   
+        print('Area of rectangle with vertices ({},{}) ({},{}) ({},{}) is {}'.format(sides[j][0],sides[j][1],sides[j][2],sides[j][3],sides[j][4],sides[j][5],areas[j]))   
 main()
